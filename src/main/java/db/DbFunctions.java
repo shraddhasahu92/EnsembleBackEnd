f92d8bf1ddd47d336f33453be8c4a4141b5191b5
Shraddha <shraddhasahu92@gmail.com>
2017-07-23 19:31:48 +0530

    commit test

--- src/main/java/db/DbFunctions.java
+++ src/main/java/db/DbFunctions.java
@@ -14,7 +14,8 @@ import java.util.concurrent.atomic.AtomicLong;
  * Created by Shraddha on 21-07-2017.
  */
 
-//Conflict Comment
+//Commit test
+    
 public class DbFunctions {
 
     public static RegisterResponse storeUser(String name, String email, String password ){
